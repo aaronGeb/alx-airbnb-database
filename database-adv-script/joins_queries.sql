f20dc73877068c34c5de6721a9dc9da5cd129aa9
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-06 14:25:33 +0300

    Add sql join basics

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -11,17 +11,17 @@ FROM bookings b
 INNER JOIN users u ON b.user_id = u.user_id;  
 
 -- LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
-SELECT  p.property_id,
-        p.name,
-        p.created_at,
-        p.updated_at,
-        p.description,
-        r.review_id,
-        r.rating,
-        r.comment,
-        r.created_at AS review_created_at
-FROM properties p
-LEFT JOIN reviews r ON p.property_id = r.property_id;
+SELECT 
+    p.property_id,
+    p.property_name,
+    p.description,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.review_date
+FROM 
+    properties p
+LEFT JOIN  reviews r ON p.property_id = r.property_id
 
 
 -- FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
